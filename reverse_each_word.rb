b3dc25e3f26c173fcecbfbbb3279d1ef317ee728
Ayleen  Nazario <ayleen.nazario@gmail.com>
2019-04-07 00:30:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word (sentence)
  first_array = sentence.split(" ")
  second_array =  []
- first_array.each do |string|
+ first_array.collect do |string|
   second_array  << string.reverse
  end
 return  second_array.join(" ")
